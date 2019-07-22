2ac5a10b9bde342436e8a31cadd953f2af39c477
cmitchell <clmitchell630@gmail.com>
2019-07-22 00:38:08 -0500

    JS turned initialization into function, set win/loss conditions

--- assets/javascipt/game.js
+++ assets/javascipt/game.js
@@ -3,31 +3,8 @@
 console.log(" (\\  (\\\n (^___^)\nc(_(\")(\")\n  Hello!");
 console.log("this is game.js");
 
-//variables
-var score = 0;
-var wins = 0;
-var loss = 0;
+init();
 
-//pick random number between 19 and 120
-var matchNum = randomRange(19, 120);
-
-//print number in #randomNumber
-$("#randomNumber").text(matchNum);
-
-//assign random number to each of the rupee buttons between 1 and 12
-//#green, #blue, #yellow, and #red
-var buttonNum = randomRange(1, 12);
-var rupees = {
-    green: 0, blue: 0, yellow: 0, red: 0
-}
-console.log(rupees);
-
-for (var key in rupees) {
-    rupees[key] = randomRange(1, 12);
-}
-
-//#score is set to 0
-$("#score").text("0");
 
 //------game------
 
@@ -37,9 +14,22 @@ $(".rupeeStyle").on("click", rupeeValue);
 /*  if #score < #randomNumber then continue playing.
     if #score = #randomNumber then you win. 
     if #score > #randomNumber then you lose "dun dun dunnnn". 
+    print result in #result
 */
 
-//print result in #result
+if (score > matchNum) {
+    loss++;
+    $("#result").text("You lose!");
+    $("#loss").text(loss);
+
+}
+else if (score === matchNum) {
+    wins++;
+    $("#result").text("You Win!");
+    $("#wins").text(wins);
+}
+
+
 
 //re-initialize
 
@@ -50,12 +40,41 @@ function randomRange(y, x) {
 
 function rupeeValue(e) {
     //add rupee value to score
+    score = score + rupees[$(this)[0].id];
+    $("#score").text(score);
+
     //console.log(e);
     console.log("I'm the rupee color: " + $(this)[0].id);
     console.log(rupees);
-    score = score + rupees[$(this)[0].id];
     console.log("I'm equal to: " + score);
-    
-    $("#score").text(score);
+}
+
+function init() {
+
+    //variables
+    var score = 0;
+    var wins = 0;
+    var loss = 0;
+
+    //pick random number between 19 and 120
+    var matchNum = randomRange(19, 120);
+
+    //print number in #randomNumber
+    $("#randomNumber").text(matchNum);
+
+    //assign random number to each of the rupee buttons between 1 and 12
+    //#green, #blue, #yellow, and #red
+    var buttonNum = randomRange(1, 12);
+    var rupees = {
+        green: 0, blue: 0, yellow: 0, red: 0
+    }
+    console.log(rupees);
+
+    for (var key in rupees) {
+        rupees[key] = randomRange(1, 12);
+    }
+
+    //#score is set to 0
+    $("#score").text("0");
 
 }
