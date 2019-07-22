b74b434c0508bf76f977102b092a4576790127a8
cmitchell <clmitchell630@gmail.com>
2019-07-22 00:14:54 -0500

    JS 'should have committed earlier' set up: random number and on(click) functions

--- assets/javascipt/game.js
+++ assets/javascipt/game.js
@@ -2,18 +2,36 @@
 //------initialization-------
 console.log(" (\\  (\\\n (^___^)\nc(_(\")(\")\n  Hello!");
 console.log("this is game.js");
+
+//variables
+var score = 0;
+var wins = 0;
+var loss = 0;
+
 //pick random number between 19 and 120
+var matchNum = randomRange(19, 120);
 
 //print number in #randomNumber
+$("#randomNumber").text(matchNum);
 
 //assign random number to each of the rupee buttons between 1 and 12
 //#green, #blue, #yellow, and #red
+var buttonNum = randomRange(1, 12);
+var rupees = {
+    green: 0, blue: 0, yellow: 0, red: 0
+}
+
+for (var key in rupees) {
+    rupees[key] = randomRange(1, 12);
+}
 
 //#score is set to 0
+$("#score").text("0");
 
 //------game------
 
 //button is clicked then adds button value to #score
+$(".rupeeStyle").on("click", rupeeValue);
 
 /*  if #score < #randomNumber then continue playing.
     if #score = #randomNumber then you win. 
@@ -24,12 +42,15 @@ console.log("this is game.js");
 
 //re-initialize
 
+//-------functions------
+function randomRange(y, x) {
+    return Math.floor(Math.random() * (x - y) + y);
+}
 
-$("#green").on("click", function () {
-    var matchNum = Math.floor(Math.random() * (120 - 19) + 19);
-    var buttonNum = Math.floor(Math.random() * (12 - 1) + 1);
-    console.log("match number is: " + matchNum);
-    console.log("button number is: " + buttonNum);
-});
+function rupeeValue(e) {
+    //add rupee value to score
+    //console.log(e);
+    //console.log("I'm the rupee color: " + $(this)[0].id);
+    score = score + rupees[$(this)[0].id];
 
-
+}
